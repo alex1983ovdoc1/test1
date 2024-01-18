bdcf96c80b7fb03bb99e07e89973f9b7b4112ffd
bondpy202212 <149815146+bondpy202212@users.noreply.github.com>
2024-01-18 18:20:28 +0200

    first commit

---
+++ fabfile.py
@@ -0,0 +1,48 @@
+# from fabric import Connection
+# from invoke import task
+
+# @task
+# def test(c):
+#     conn = Connection(host='34.82.228.195', user='bondar1983ovdoc1', connect_kwargs={"key_filename": "C:/.ssh/id_rsa"})
+#     conn.run('date')
+#     conn.local('date')
+
+
+
+# import os
+from fabric.api import env, settings, run, local, cd, sudo
+
+from config import mIP, mNAME, mADRESSVM
+
+
+
+
+# env.hosts = [vm_ip]
+# env.user = user_name
+
+
+env.hosts = [str(mIP)]
+env.user = str(mNAME)
+
+# current_directory = os.path.dirname(os.path.abspath(__file__))
+# adress_filename = os.path.join(current_directory, '.ssh/id_rsa')
+# adress_filename = str(adress_filename.replace("\\", "/"))
+# print(adress_filename)
+# env.key_filename = adress_filename
+
+# env.key_filename = "C:/.ssh/id_rsa"
+
+env.reject_unknown_hosts = False
+env.disable_known_hosts = True
+
+def test():
+    with settings(connect_kwargs={"key_filename": "C:/Users/Alex01/.ssh/id_rsa"}, warn_only=True):
+        run("date")
+        local("date")
+
+def update():
+	# with cd(mADRESSVM):	
+	with cd(str(mADRESSVM)):		
+		run("git stash")
+		run("git pull")
+		sudo("supervisorctl restart flask")
